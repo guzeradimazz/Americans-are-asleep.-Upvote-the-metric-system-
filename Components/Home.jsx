7db48739419a45bacbaba548e4a792546deca63d
guzeradimazz <Guzerashop@mail.ru>
2022-09-04 20:08:43 +0300

    desing fixing

--- Components/Home.jsx
+++ Components/Home.jsx
@@ -1,4 +1,4 @@
-import react from 'react'
+import react, { useState } from 'react'
 import { View, Text, Pressable, StyleSheet } from 'react-native'
 
 export const Home = ({ navigation }) => {
@@ -12,6 +12,10 @@ export const Home = ({ navigation }) => {
         navigation.navigate('Temperature')
     }
 
+    const [isPremium, SetIsPremium] = useState(false)
+    const getPremium = () => {
+        isPremium ? SetIsPremium(false) : SetIsPremium(true)
+    }
     return (
         <View style={styles.wrapper}>
             <View style={styles.nav}>
@@ -30,6 +34,13 @@ export const Home = ({ navigation }) => {
                 <Text style={styles.mainTxt2}>
                     Click on any tab below and convert values...
                 </Text>
+            </View>
+            <View style={styles.wrapperBtn}>
+                <Pressable style={isPremium ? styles.btnPremiumFull : styles.btnPremium} onPress={getPremium}>
+                    <Text style={styles.btnPremiumTxt}>
+                        {isPremium ? 'Off Premium' : 'Take Premium'}
+                    </Text>
+                </Pressable>
             </View>
         </View>
     )
@@ -66,11 +77,47 @@ const styles = StyleSheet.create({
         width: '30%',
         height: 40,
         justifyContent: 'center',
-        borderRadius: 5
+        borderRadius: 5,
+        shadowColor:'#999',
+        shadowOffset:{width:0,height:0},
+        shadowOpacity:1,
+        shadowRadius:10
     },
     navItemTxt: {
         color: '#fff',
         textAlign: 'center',
         fontSize: 20
+    },
+    wrapperBtn: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 100
+    },
+    btnPremium: {
+        
+        backgroundColor: '#999',
+        width: '50%',
+        height: 50,
+        justifyContent: 'center',
+        borderRadius: 5,
+        shadowColor:'#999',
+        shadowOffset:{width:0,height:0},
+        shadowOpacity:1,
+        shadowRadius:10
+    },
+    btnPremiumFull:{
+        shadowColor:'#f2c11f',
+        shadowOffset:{width:0,height:0},
+        shadowOpacity:1,
+        shadowRadius:10,
+        backgroundColor: '#f2c11f',
+        width: '50%',
+        height: 50,
+        justifyContent: 'center',
+        borderRadius: 5
+    },
+    btnPremiumTxt: {
+        textAlign: 'center',
+        fontSize: 26
     }
 })
