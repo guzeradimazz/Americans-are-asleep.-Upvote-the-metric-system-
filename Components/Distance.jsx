466efa791489217f96e15044def6b1d70829d11e
guzeradimazz <Guzerashop@mail.ru>
2022-09-09 18:36:48 +0300

    copy function added

--- Components/Distance.jsx
+++ Components/Distance.jsx
@@ -1,6 +1,14 @@
 import react, { useState, useEffect } from 'react'
-import { View, Text, TextInput, StyleSheet } from 'react-native'
+import {
+    View,
+    Text,
+    TextInput,
+    StyleSheet,
+    Pressable,
+    Image
+} from 'react-native'
 import { DropdownComponent } from './DropdownComponent'
+import * as Clipboard from 'expo-clipboard'
 import { isNumber } from '../utils/isNumber'
 
 export const Distance = ({ isPremium }) => {
@@ -17,6 +25,14 @@ export const Distance = ({ isPremium }) => {
         { label: 'Cantimeters', amount: 0.01, value: 2 },
         { label: 'Millimeters', amount: 0.001, value: 3 }
     ]
+
+    const copyToClipboard1 = async () => {
+        await Clipboard.setStringAsync(value1.toString())
+    }
+
+    const copyToClipboard2 = async () => {
+        await Clipboard.setStringAsync(value2.toString())
+    }
 
     const [value1, setValue1] = useState('')
     const [value2, setValue2] = useState('')
@@ -54,22 +70,49 @@ export const Distance = ({ isPremium }) => {
                 setValue={setOption1}
                 isPremium={isPremium}
             />
-            <TextInput
-                maxLength={15}
-                placeholder='Enter value here'
-                style={styles.input}
-                value={value1.toString()}
-                onChangeText={
-                    isPremium
-                        ? option1 && option2
+
+            <View
+                style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    width: '90%'
+                }}
+            >
+                <TextInput
+                    maxLength={15}
+                    placeholder='Enter value here'
+                    style={isPremium ? styles.inputPremium : styles.input}
+                    value={value1.toString()}
+                    onChangeText={
+                        isPremium
+                            ? option1 && option2
+                                ? onChangeInput1
+                                : errDistance
+                            : option1
                             ? onChangeInput1
                             : errDistance
-                        : option1
-                        ? onChangeInput1
-                        : errDistance
-                }
-                keyboardType='numeric'
-            ></TextInput>
+                    }
+                    keyboardType='numeric'
+                ></TextInput>
+
+                {isPremium ? (
+                    <Pressable onPress={copyToClipboard1}>
+                        <Image
+                            style={{
+                                right: 0,
+                                marginRight: 10,
+                                marginTop: 10,
+                                width: 30,
+                                height: 30
+                            }}
+                            source={{
+                                uri: 'https://cdn-icons-png.flaticon.com/512/4280/4280618.png'
+                            }}
+                        />
+                    </Pressable>
+                ) : null}
+            </View>
             {isPremium ? (
                 <DropdownComponent
                     onChangeInput={onChangeInput2}
@@ -82,25 +125,64 @@ export const Distance = ({ isPremium }) => {
             ) : null}
             <View style={{ width: '100%', alignItems: 'center' }}>
                 {isPremium ? (
-                    <TextInput
-                        placeholder='Meters'
-                        style={styles.input}
-                        value={value2.toString()}
-                        onChangeText={
-                            option2 && option1 ? onChangeInput2 : errDistance
-                        }
-                        keyboardType='numeric'
-                    ></TextInput>
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                            width: '90%'
+                        }}
+                    >
+                        <TextInput
+                            placeholder='Enter value here'
+                            style={
+                                isPremium ? styles.inputPremium : styles.input
+                            }
+                            value={value2.toString()}
+                            onChangeText={
+                                option2 && option1
+                                    ? onChangeInput2
+                                    : errDistance
+                            }
+                            keyboardType='numeric'
+                        ></TextInput>
+
+                        <Pressable onPress={copyToClipboard2}>
+                            <Image
+                                style={{
+                                    right: 0,
+                                    marginRight: 10,
+                                    marginTop: 10,
+                                    width: 30,
+                                    height: 30
+                                }}
+                                source={{
+                                    uri: 'https://cdn-icons-png.flaticon.com/512/4280/4280618.png'
+                                }}
+                            />
+                        </Pressable>
+                    </View>
                 ) : (
                     <Text>{value1 ? +value1 * option1.amount : ''}</Text>
                 )}
-                <Text style={{ textAlign: 'center' }}>Meters</Text>
+                {isPremium ? null : (
+                    <Text style={{ textAlign: 'center' }}>Meters</Text>
+                )}
             </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    inputPremium: {
+        height: 50,
+        textAlign: 'center',
+        width: '70%',
+        borderBottomColor: '#000',
+        borderBottomWidth: 2,
+        borderRadius: 5,
+        fontSize: 20
+    },
     wrapper: {
         flex: 1,
         justifyContent: 'space-around',
--- Components/Temperature.jsx
+++ Components/Temperature.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Pressable, Image } from 'react-native'
+import * as Clipboard from 'expo-clipboard'
 import { DropdownComponent } from './DropdownComponent'
 import { TextInput } from 'react-native'
 import { isNumber } from '../utils/isNumber'
@@ -45,6 +46,15 @@ export const Temperature = ({ isPremium }) => {
             value: 2
         }
     ]
+
+    const copyToClipboard1 = async () => {
+        await Clipboard.setStringAsync(value1.toString())
+    }
+
+    const copyToClipboard2 = async () => {
+        await Clipboard.setStringAsync(value2.toString())
+    }
+
     const [value1, setValue1] = useState('')
     const [value2, setValue2] = useState('')
 
@@ -85,22 +95,47 @@ export const Temperature = ({ isPremium }) => {
                 setValue={setOption1}
                 isPremium={isPremium}
             />
-            <TextInput
-                maxLength={15}
-                placeholder='Enter value here'
-                style={styles.input}
-                value={value1.toString()}
-                onChangeText={
-                    isPremium
-                        ? option1 && option2
+            <View
+                style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    width: '90%'
+                }}
+            >
+                <TextInput
+                    maxLength={15}
+                    placeholder='Enter value here'
+                    style={isPremium ? styles.inputPremium : styles.input}
+                    value={value1.toString()}
+                    onChangeText={
+                        isPremium
+                            ? option1 && option2
+                                ? onChangeInput1
+                                : errDistance
+                            : option1
                             ? onChangeInput1
                             : errDistance
-                        : option1
-                        ? onChangeInput1
-                        : errDistance
-                }
-                keyboardType='numeric'
-            ></TextInput>
+                    }
+                    keyboardType='numeric'
+                ></TextInput>
+                {isPremium ? (
+                    <Pressable onPress={copyToClipboard1}>
+                        <Image
+                            style={{
+                                right: 0,
+                                marginRight: 10,
+                                marginTop: 10,
+                                width: 30,
+                                height: 30
+                            }}
+                            source={{
+                                uri: 'https://cdn-icons-png.flaticon.com/512/4280/4280618.png'
+                            }}
+                        />
+                    </Pressable>
+                ) : null}
+            </View>
             {isPremium ? (
                 <DropdownComponent
                     onChangeInput={onChangeInput2}
@@ -113,25 +148,64 @@ export const Temperature = ({ isPremium }) => {
             ) : null}
             <View style={{ width: '100%', alignItems: 'center' }}>
                 {isPremium ? (
-                    <TextInput
-                        placeholder={option2?.label}
-                        style={styles.input}
-                        value={value2.toString()}
-                        onChangeText={
-                            option2 && option1 ? onChangeInput2 : errDistance
-                        }
-                        keyboardType='numeric'
-                    ></TextInput>
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                            width: '90%'
+                        }}
+                    >
+                        <TextInput
+                            placeholder={option2?.label || 'Enter value here'}
+                            style={
+                                isPremium ? styles.inputPremium : styles.input
+                            }
+                            value={value2.toString()}
+                            onChangeText={
+                                option2 && option1
+                                    ? onChangeInput2
+                                    : errDistance
+                            }
+                            keyboardType='numeric'
+                        ></TextInput>
+
+                        <Pressable  onPress={copyToClipboard2}>
+                            <Image
+                                style={{
+                                    right: 0,
+                                    marginRight: 10,
+                                    marginTop: 10,
+                                    width: 30,
+                                    height: 30
+                                }}
+                                source={{
+                                    uri: 'https://cdn-icons-png.flaticon.com/512/4280/4280618.png'
+                                }}
+                            />
+                        </Pressable>
+                    </View>
                 ) : (
                     <Text>{value1 ? option1.converterOut(+value1) : ''}</Text>
                 )}
-                <Text style={{ textAlign: 'center' }}>Celsium</Text>
+                {isPremium ? null : (
+                    <Text style={{ textAlign: 'center' }}>Celsium</Text>
+                )}
             </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    inputPremium: {
+        height: 50,
+        textAlign: 'center',
+        width: '70%',
+        borderBottomColor: '#000',
+        borderBottomWidth: 2,
+        borderRadius: 5,
+        fontSize: 20
+    },
     wrapper: {
         flex: 1,
         justifyContent: 'space-around',
